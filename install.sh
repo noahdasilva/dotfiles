80657f40d1f0a7fa69d56e5efbf6bab524f80c51
Noah Da Silva <noah@noahdasilva.com>
2026-04-03 18:18:52 -0400

    Added ssh config and symlink install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+ln -sf ~/dotfiles/ssh/config ~/.ssh/config
+ln -sf ~/dotfiles/tmux/tmux.conf ~/.config/tmux/tmux.conf
+ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
